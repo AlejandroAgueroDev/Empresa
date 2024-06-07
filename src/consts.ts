94fbfd6e253c12b2a839b8eba4557c0e03cb6edc
Alejandro <alejandroaguerodev@gmail.com>
2024-06-07 16:12:35 -0300

    chequear responsive de navbar

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Custodia De Archivos Noroeste SRL';
+export const SITE_DESCRIPTION = 'Bienvenido a nuestro sitio web!';
