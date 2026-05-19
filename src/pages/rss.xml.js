330a4b2ffe2a7f4ccf96fd35dacbb042d7fa0569
AlejandroAgueroDev <alejandroaguerodev@gmail.com>
2026-05-18 21:01:40 -0300

    fix 2

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,20 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("blog").catch(() => []);
+  if (!posts.length)
+    return new Response('<rss version="2.0"><channel></channel></rss>', {
+      headers: { "Content-Type": "application/xml" },
+    });
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
 }
